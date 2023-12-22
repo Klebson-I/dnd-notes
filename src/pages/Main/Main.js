31339b810b9a10f2cc91d57940d0436107e3d302
lkleba <lukasz.kleba.ext@nokia.com>
2023-12-22 11:46:14 +0100

    create draft of NoteTable

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,11 @@
 import { Grid } from "@mui/material";
 import PageHeader from "../../components/PageHeader/PageHeader";
+import NoteTable from "../../components/NoteTable/NoteTable";
 
 const Main = () => {
     return <Grid container spacing={12}>
         <PageHeader />
+        <NoteTable />
     </Grid>
 }
 
