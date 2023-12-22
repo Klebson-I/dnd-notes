d1674e1110c75eeda6700c8e428a29583fd599d3
lkleba <lukasz.kleba.ext@nokia.com>
2023-12-22 11:35:43 +0100

    change background

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,16 +1,16 @@
-import { Grid } from "@mui/material";
+import { Grid, Paper } from "@mui/material";
 import EditNoteIcon from '@mui/icons-material/EditNote';
 import './style.css';
 
 
 const PageHeader = () => {
     return <Grid item xs={12}>
-        <div className="headerContainerPaper">
+        <Paper className="headerContainerPaper">
             <h1 className="headerText">
                 DND Note App
             </h1>
             <EditNoteIcon sx={{fontSize: '3rem'}}/>
-        </div>
+        </Paper>
     </Grid>
 }
 
